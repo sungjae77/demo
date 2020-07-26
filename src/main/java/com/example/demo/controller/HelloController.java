2c8784f3d51d8c8856b05159d48762b2b984e728
wisdomguy <wisdomguy@naver.com>
2020-07-27 02:25:40 +0900

    add hello2

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @RequestMapping("/hello2")
+    public String hello2(){
+        return "hello2";
+    }
 }
